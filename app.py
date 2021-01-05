74f171ce218d386900b5e6b51e2156c99e29bbe2
Noob Scorpio <aliadam7867@gmail.com>
2021-01-05 09:50:13 +0500

    TEST2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objs as go
 import re
 import dash
 import dash_bootstrap_components as dbc
-# from APIS import invoices
+from APIS import invoices
 # from APIS import items
 # from APIS import payments
 # from APIS import reports
